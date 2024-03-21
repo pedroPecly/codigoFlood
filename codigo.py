0d2ea6a113a9f1267f681ddda54e1d79b9cfd060
pedroPecly <pedrohpg23@hotmail.com>
2024-03-20 23:45:00 -0300

    adicionando a funcao de tempo

--- codigo.py
+++ codigo.py
@@ -4,15 +4,23 @@ import keyboard
 import tkinter as tk
 from tkinter import Entry, Button, Label
 
-def digitar_texto_rapido(texto, velocidade):
+def digitar_texto_rapido(texto, velocidade, tempo=None):
     time.sleep(5)
-
+    tempo_inicial = time.time()
+    
     while True:
+        tempo_atual = time.time()
+        tempo_decorrido = tempo_atual - tempo_inicial
+        
         pyautogui.typewrite(texto, interval=0.01)
 
         pyautogui.press('enter')
 
         time.sleep(velocidade)
+        
+        if tempo is not None and tempo_decorrido >= tempo:
+            print("Tempo esgotado.")
+            break
         
         if keyboard.is_pressed('='):
             print("Parou")
@@ -20,17 +28,27 @@ def digitar_texto_rapido(texto, velocidade):
 
 def iniciar_digitar():
     texto = entrada_texto.get()
-    velocidade = entrada_velocidade.get()
-    digitar_texto_rapido(texto, float(velocidade))
+    velocidade = float(entrada_velocidade.get())
+    tempo_str = entrada_tempo.get()
+    
+    if tempo_str.strip() == "":
+        digitar_texto_rapido(texto, velocidade)
+    else:
+        tempo = float(tempo_str)
+        digitar_texto_rapido(texto, velocidade, tempo)
 
 root = tk.Tk()
 root.title("Encherdor de saco")
-
 
 label_velocidade = Label(root, text="Velocidade:")
 label_velocidade.pack(pady=5)
 entrada_velocidade = Entry(root, width=40)
 entrada_velocidade.pack(pady=10)
+
+label_tempo = Label(root, text="Tempo:")
+label_tempo.pack(pady=5)
+entrada_tempo = Entry(root, width=40)
+entrada_tempo.pack(pady=5)
 
 label_texto = Label(root, text="Texto:")
 label_texto.pack(pady=5)
